9d19dd37bc03581b860e1c511d617f4662fa221b
Jennifer <115043624+jenn0112@users.noreply.github.com>
2022-11-21 22:09:45 -0800

    a4- commit

--- source/analysis.R
+++ source/analysis.R
@@ -20,31 +20,79 @@ test_query2 <- function(num=6) {
 
 ## Section 2  ---- 
 #----------------------------------------------------------------------------#
-# Your functions and variables might go here ... <todo: update comment>
+# library("dplyr")
+library("dplyr")
+library("tidyr")
+library("ggplot2")
+incarceration <- read.csv("https://raw.githubusercontent.com/vera-institute/incarceration-trends/master/incarceration_trends.csv")
 #----------------------------------------------------------------------------#
-
+get_data <- function(num_records=-1) {
+  fname <- "~/Documents/info201/data/incarceration_trends.csv"
+  df <- read.csv(fname, nrows=num_records)
+  return(df)
+}
 ## Section 3  ---- 
 #----------------------------------------------------------------------------#
 # Growth of the U.S. Prison Population
 # Your functions might go here ... <todo:  update comment>
 #----------------------------------------------------------------------------#
 # This function ... <todo:  update comment>
-get_year_jail_pop <- function() {
-  # TODO: Implement this function 
-return()   
+get_year_jail_pop <-function(df) {
+  t <- incarceration_df %>%
+    group_by(state) %>%
+    summarise(p = sum(total_jail_pop, na.rm = TRUE)) %>%
+    filter(p == 0) %>%
+    select(state, p) %>%
+    pull(state)
+  return(t)
 }
 
 # This function ... <todo:  update comment>
-plot_jail_pop_for_us <- function()  {
-  # TODO: Implement this function 
-  return()   
-} 
+plot_jail_pop_for_us <- function(df) {
+  t <- incarceration_df %>%
+    group_by(state) %>%
+    summarise(p = sum(total_jail_pop, na.rm = TRUE)) %>%
+    filter(p == 0) %>%
+    select(state, p) %>%
+    pull(state)
+  return(t)
+}
+
+View(plot_jail_pop_for_us)
 
 ## Section 4  ---- 
 #----------------------------------------------------------------------------#
 # Growth of Prison Population by State 
 # Your functions might go here ... <todo:  update comment>
 # See Canvas
+get_data <- function(num_records=-1) {
+  fname <- "~/Documents/info201/data/incarceration_trends.csv"
+  df <- read.csv(fname, nrows=num_records)
+  return(df)
+}
+
+get_jail_pop_by_states <-function(df) {
+  t <- incarceration_df %>%
+    group_by(state) %>%
+    summarise(p = sum(total_jail_pop, na.rm = TRUE)) %>%
+    filter(p == 0) %>%
+    select(state, p) %>%
+    pull(state)
+  return(t)
+}
+
+plot_jail_pop_by_states <- function(df) {
+  t <- incarceration_df %>%
+    group_by(state) %>%
+    summarise(p = sum(total_jail_pop, na.rm = TRUE)) %>%
+    filter(p == 0) %>%
+    select(state, p) %>%
+    pull(state)
+  return(t)
+}
+
+View(get_jail_pop_by_states)
+
 #----------------------------------------------------------------------------#
 
 ## Section 5  ---- 
@@ -52,6 +100,25 @@ plot_jail_pop_for_us <- function()  {
 # <variable comparison that reveals potential patterns of inequality>
 # Your functions might go here ... <todo:  update comment>
 # See Canvas
+get_basic_info <- function(df) {
+  t <- "Basic Info on `incarceration_trends`\n"
+  t <- paste0(t, "  No. regions               ", length(unique(df$region)), "\n")
+  t <- paste0(t, "  No. divisions             ", length(unique(df$division)), "\n")
+  t <- paste0(t, "  No. states                ", length(unique(df$state)), "\n")
+  t <- paste0(t, "  No. counties              ", length(unique(df$county_name)), "\n")
+  t <- paste0(t, "  No. fips (county IDs)     ", length(unique(df$fips)), "\n")
+  t <- paste0(t, "  No. different urbanicity  ", length(unique(df$urbanicity)), "\n")
+  
+  t <- paste0(t, "Divisions ...\n")
+  t <- paste0(t, format_region_info("Midwest"), "\n")
+  t <- paste0(t, format_region_info("Northeast"), "\n")
+  t <- paste0(t, format_region_info("South"), "\n")
+  t <- paste0(t, format_region_info("West"), "\n")
+  
+  cat(t) 
+}
+
+Some races are disproportionally represented in jail in some areas of the country, it shows that the means number of incarcerations for different races go from white races having the highest mean of incarcerations, to black being next, then latinx, then native, then natives having the smallest mean of incarcerations.
 #----------------------------------------------------------------------------#
 
 ## Section 6  ---- 
